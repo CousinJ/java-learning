af059d071e5f852c245cd418e8042249cfe89706
Jason Davis <Jasonodavis323@gmail.com>
2023-02-19 00:27:56 -0700

    first commit

---
+++ Calculate.java
@@ -0,0 +1,14 @@
+public class Calculate {
+    public static void main(String[] args) {
+        int num_1 = 5;
+        int num_2 = 2;
+        int num_3 = 10;
+        int num_4 = 3;
+
+         System.out.println(calculation_1(num_1, num_2, num_3));
+    }
+
+    public static int calculation_1(int x, int y, int z) {
+        return (x * z) + y;
+    }
+}
